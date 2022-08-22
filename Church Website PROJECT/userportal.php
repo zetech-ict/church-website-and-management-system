79b5e5882d8a938b9974c851205201adaafb9ec1
Michael Oluoch <78139141+mike-mike-8000@users.noreply.github.com>
2022-08-22 14:48:49 +0300

    Add files via upload

--- Church Website PROJECT/userportal.php
+++ Church Website PROJECT/userportal.php
@@ -13,7 +13,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css>s"
+    <link rel="stylesheet" href="style.css">
     <!-- Latest compiled and minified CSS -->
 <!-- CSS only -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
@@ -23,11 +23,10 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 <title>User Portal</title>
 </head>
-
-<header class="header">
-    <a id="adm">User Portal</a>
+<header>
     <nav>
         <ul>
+            <li><a href="userportal.php">User Home</a></li>
             <li><a href="announce.php">Announcements</a></li>
             <li><a href="about.php">About</a></li>
             <li><a href="contact.php">Contact Us</a></li>
